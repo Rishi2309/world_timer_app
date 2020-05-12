053ee2b901a14ea93960ce101617a413770c95e6
Rishi2309 <34580753+Rishi2309@users.noreply.github.com>
2020-05-12 23:28:30 +0530

    Starting Flutter App

---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class chooseLocation extends StatefulWidget {
+  @override
+  _chooseLocationState createState() => _chooseLocationState();
+}
+
+class _chooseLocationState extends State<chooseLocation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Choose a location"),
+    );
+  }
+}
