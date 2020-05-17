a6593e7505a9accf43fe44681a6c3d07beda0889
Rishi2309 <34580753+Rishi2309@users.noreply.github.com>
2020-05-17 22:35:19 +0530

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:world_timer_app/pages/choose_location.dart';
 
 
 void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
   routes: {
     '/': (context) => Loading(),
     '/home': (context) => Home(),
